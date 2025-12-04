c04447bdc8a38a0295625a4c87b2fd60986fcf81
Mathilde1420 <kassemathilde@gmail.com>
2025-12-04 15:46:04 +0000

    maj

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   trailingSlash: true,
+  images: {
+    unoptimized: true, // <-- crucial pour next export
+    // Si tu utilises des images externes, tu peux ajouter des domaines :
+    // domains: ['example.com'],
+    // ou remotePatterns: [...]
+  },
 };
 
 export default nextConfig;
