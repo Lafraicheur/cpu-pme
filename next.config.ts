b6da1c0ec8622895f38fe48e37a5e055da5ec535
Lafraicheur <86606431+Lafraicheur@users.noreply.github.com>
2026-01-11 19:54:33 +0000

    Restore output option in next.config.ts
    Restored output option to allow API routes usage.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export", // Retiré pour permettre l'utilisation des routes API
+  output: "export", // Retiré pour permettre l'utilisation des routes API
   trailingSlash: false, // Désactivé pour les appels API (pas de slash à la fin)
   images: {
     unoptimized: true,
